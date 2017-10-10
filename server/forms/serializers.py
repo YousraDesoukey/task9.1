a55ec44c0039768db447580f5f60d2594e6576f2
YousraDesoukey <yousradesoukey@hotmail.com>
2017-10-10 04:14:58 +0200

    I used postgresql DB and tried some validations in serializers.py

--- server/forms/serializers.py
+++ server/forms/serializers.py
@@ -5,4 +5,26 @@ from .models import Forms
 class FormsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Forms
-        fields = '__all__'
+        ProfilePicture = serializers.CharField(max_length=1000)
+        Name = serializers.CharField(max_length=100)
+        Email = serializers.EmailField(max_length=500)
+        Mobile = serializers.CharField(length=12)
+        National_ID = serializers.CharField(max_length=40)
+        University = serializers.CharField(max_length=100)
+        Faculty = serializers.CharField(max_length=100)
+        Major = serializers.CharField(max_length=100)
+        Minor = serializers.CharField(max_length=100)
+        Expected_GraduationYear = serializers.CharField(max_length=4)
+
+        def validate_email(self, value):
+
+            if '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$' not in value:
+                raise serializers.ValidationError("Your Email is invalid")
+            return value
+
+
+        def validate_mobile(self, value):
+
+            if '^(\+\d{1,2}\s)?\(?\d{4}\)?[\s.-]\d{4}[\s.-]\d{4}$' not in value:
+                raise serializers.ValidationError("Please Enter 12 digits")
+            return value
--- server/task9/settings.py
+++ server/task9/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'task9.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.path.join(BASE_DIR, 'ASURT18'),
+        'USER': 'ITians',
+        'PASSWORD': 'ITians@ASURT',
+        'HOST': 'localhost',
+        'PORT': '',
+
     }
 }
 
